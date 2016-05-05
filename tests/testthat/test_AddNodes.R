07bdff07b416c48d0f740385d68c1346315109d0
unknown <unknown@unknown.com>
2016-05-05 11:38:21 -0400

    Added test for error if single node tree does not contain the parent.

--- tests/testthat/test_AddNodes.R
+++ tests/testthat/test_AddNodes.R
@@ -14,5 +14,11 @@ test_that("add child to empty tree works", {
  child1 <- root$add_node(0, 1, "child1")
 
  expect_is(child1, "GeneralTree")
+})
 
+test_that("adding a child with an non existing parent results in failure", {
+  require(GeneralTree)
+
+ root <- GeneralTree$new(0, "parent1")
+ expect_error(root$add_node(2, 1, "child1"))
 })
