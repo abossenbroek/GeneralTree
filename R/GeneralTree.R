008dffabac8c103878f788eab1c814896cbb350c
Anton Bossenbroek <anton.bossenbroek@me.com>
2016-05-05 17:18:52 -0400

    Completed adding multiple child nodes and siblings.

--- R/GeneralTree.R
+++ R/GeneralTree.R
@@ -20,7 +20,13 @@ GeneralTree <- R6Class('GeneralTree',
      invisible(self)
    },
    add_node = function(parent_id, id, data) {
-     # i.   tree contains only root node
+     new_node = NULL
+
+     # Find the parent node.
+     parent_node = self$search_id(parent_id)
+
+     if (is.null(parent_node)) stop("Could not find the parent node with id ", id)
+
      if (is.null(private$.root) && is.null(private$.siblings) &&
          is.null(private$.left_child)) {
        if (parent_id != private$.id) {
@@ -28,16 +34,26 @@ GeneralTree <- R6Class('GeneralTree',
        } else {
          # Add the child and set up all the references in the child correctly.
          private$.left_child = GeneralTree$new(id, data)
-         private$.left_child$set_root(self)
-         added_node = private$.left_child
+         private$.left_child$set_root(parent_node)
+         new_node = private$.left_child
        }
      } else {
-       # Find the parent node.
-       parent_node = self$search_id(parent_id)
+       if (!is.null(parent_node)) {
+         new_node = GeneralTree$new(id, data)
 
-       #TODO: implement
+         # Verify whether any childs are already present.
+         if (parent_node$have_child) {
+           parent_node$left_child$add_sibling(new_node)
+         } else {
+           parent_node$set_left_child(new_node)
+           new_node$set_root(parent_node$root)
+         }
+       } else {
+         stop("Could not find matching parent node with parent id ", parent_id)
+       }
      }
-     invisible(added_node)
+     #browser()
+     invisible(new_node)
    },
    search_id = function(id) {
      # Determine whether search was called at the root node.
@@ -48,15 +64,24 @@ GeneralTree <- R6Class('GeneralTree',
 
      invisible(result)
    },
+   add_sibling = function(node) {
+     private$.siblings = c(private$.siblings, list(node))
+     node$set_root(self$root)
+
+     invisible(node)
+   },
    search_id_starting_at_node = function(id) {
      result = NULL
      # Verify whether the current node matches the id.
-     if (identical(id, self$id)) {
+     if (identical(id, private$.id)) {
        result = self
      }
 
      if (!is.null(private$.siblings) && is.null(result)) {
-       result = self$get_sibling(id)
+       for (s in private$.siblings) {
+         result = s$search_id_starting_at_node(id)
+         if (!is.null(result)) break
+       }
      }
 
      if (is.null(result)) {
@@ -85,14 +110,16 @@ GeneralTree <- R6Class('GeneralTree',
 
      invisible(result)
    },
-   add_sibling = function(node) {
-
-   },
-   add_child = function(node) {
-
-   },
    set_root = function(node) {
      private$.root = node
+   },
+   set_left_child = function(node) {
+     if (self$have_child) stop("Already have left child!")
+
+     private$.left_child = node
+   },
+   set_data = function(data) {
+     private$.data = data
    }
   ),
   active = list(
@@ -104,7 +131,7 @@ GeneralTree <- R6Class('GeneralTree',
     left_child = function() {
       invisible(private$.left_child)
     },
-    siblings= function() {
+    siblings = function() {
       invisible(private$.siblings)
     },
     id = function() {
@@ -115,6 +142,9 @@ GeneralTree <- R6Class('GeneralTree',
     },
     have_siblings = function() {
       !is.null(private$.siblings)
+    },
+    data = function() {
+      return(private$.data)
     }
   )
 )
--- tests/testthat/test_AddNodes.R
+++ tests/testthat/test_AddNodes.R
@@ -1,36 +1,69 @@
-context("Adding nodes")
-test_that("creating an empty tree works", {
-  root <- GeneralTree$new(0, "parent1")
+context('Adding nodes')
+test_that('creating an empty tree works', {
+  root <- GeneralTree$new(0, 'parent1')
 
-  expect_is(root, "GeneralTree")
+  expect_is(root, 'GeneralTree')
   expect_identical(root$left_child, NULL)
   expect_identical(root$sibling, NULL)
 })
 
-test_that("add child to empty tree works", {
- root <- GeneralTree$new(0, "parent1")
- child1 <- root$add_node(0, 1, "child1")
+test_that('add child to empty tree works', {
+ root <- GeneralTree$new(0, 'parent1')
+ child1 <- root$add_node(0, 1, 'child1')
 
- expect_is(child1, "GeneralTree")
+ expect_is(child1, 'GeneralTree')
  expect_identical(root$left_child, child1)
  expect_identical(child1$root, root)
 })
 
-test_that("adding a child with an non existing parent results in failure", {
-  root <- GeneralTree$new(0, "parent1")
-  expect_error(root$add_node(2, 1, "child1"))
+test_that('adding a child with an non existing parent results in failure', {
+  root <- GeneralTree$new(0, 'parent1')
+  expect_error(root$add_node(2, 1, 'child1'))
 })
 
-test_that("have_siblign action functions is working as expected", {
+test_that('have_siblign and have_child action functions is working as expected', {
   nodes <- list()
 
-  nodes[["root"]] <- GeneralTree$new(0, "parent1")
+  nodes[['root']] <- GeneralTree$new(0, 'parent1')
 
-  expect_identical(nodes[["root"]]$have_siblings, FALSE)
-  expect_identical(nodes[["root"]]$have_child, FALSE)
+  expect_identical(nodes[['root']]$have_siblings, FALSE)
+  expect_identical(nodes[['root']]$have_child, FALSE)
 
-  nodes[["child1"]] <- nodes[["root"]]$add_node(0, 1, "child1")
+  nodes[['child1']] <- nodes[['root']]$add_node(0, 1, 'child1')
 
-  expect_identical(nodes[["root"]]$have_child, TRUE)
+  expect_identical(nodes[['root']]$have_child, TRUE)
+})
+
+test_that('add multiple childeren', {
+  nodes <- list()
+
+  nodes[['root']] <- GeneralTree$new(0, 'parent1')
+  nodes[['child1']] <- nodes[['root']]$add_node(0, 1, 'child1')
+  nodes[['child2']] <- nodes[['root']]$add_node(0, 2, 'child2')
+
+  nodes[['child2.3']] <- nodes[['root']]$add_node(2, 3, 'child2.3')
+
+  nodes[['root']]$search_id(3)$data
+
+  expect_identical(nodes[['root']]$search_id(3)$data, 'child2.3')
+})
+
+test_that('add multiple childeren as part of child', {
+  nodes <- list()
+
+  nodes[['root']] <- GeneralTree$new(0, 'parent1')
+  nodes[['child1']] <- nodes[['root']]$add_node(0, 1, 'child1')
+  nodes[['child2']] <- nodes[['root']]$add_node(0, 2, 'child2')
+
+  nodes[['child2.3']] <- nodes[['root']]$add_node(2, 3, 'child2.3')
+
+  nodes[['child2.4']] <- nodes[['root']]$add_node(2, 4, 'child2.4')
+
+
+  nodes[['child3.5']] <- nodes[['child2.4']]$add_node(3, 5, 'child3.5')
+  nodes[['child3.5']] <- nodes[['child2.4']]$add_node(3, 5, 'child3.5')
+
+  expect_identical(nodes[['child2.4']]$search_id(5)$data, 'child3.5')
+  expect_identical(nodes[['child1']]$search_id(5)$data, 'child3.5')
 })
 
