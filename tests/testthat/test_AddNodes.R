5c563f0695e63075adb0bd655a148cf909d883dc
unknown <unknown@unknown.com>
2016-05-05 11:53:18 -0400

    Added tests of active methods.

--- tests/testthat/test_AddNodes.R
+++ tests/testthat/test_AddNodes.R
@@ -5,6 +5,8 @@ test_that("creating an empty tree works", {
   root <- GeneralTree$new(0, "parent1")
 
   expect_is(root, "GeneralTree")
+  expect_identical(root$left_child, NULL)
+  expect_identical(root$sibling, NULL)
 })
 
 test_that("add child to empty tree works", {
@@ -14,6 +16,8 @@ test_that("add child to empty tree works", {
  child1 <- root$add_node(0, 1, "child1")
 
  expect_is(child1, "GeneralTree")
+ expect_identical(root$left_child, child1)
+ expect_identical(child1$root, root)
 })
 
 test_that("adding a child with an non existing parent results in failure", {
@@ -22,3 +26,4 @@ test_that("adding a child with an non existing parent results in failure", {
  root <- GeneralTree$new(0, "parent1")
  expect_error(root$add_node(2, 1, "child1"))
 })
+
