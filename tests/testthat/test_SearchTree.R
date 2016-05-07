dd1b80f9f8baf96d9a21007ab78c4d8dd02a470c
Anton Bossenbroek <anton.bossenbroek@me.com>
2016-05-07 00:50:40 -0400

    Resolved bug in search test.

--- tests/testthat/test_SearchTree.R
+++ tests/testthat/test_SearchTree.R
@@ -1,6 +1,8 @@
 context('Searching tree')
 test_that('search tree with only a single child', {
-  tree <- GeneralTree$new(0, 'parent1')
+  nodes <- list()
+
+  nodes[['root']] <- GeneralTree$new(0, 'parent1')
   nodes[['child1']] <- nodes[['root']]$add_node(0, 1, 'child1')
   search_result <- nodes[['child1']]$search_node(1)
 
