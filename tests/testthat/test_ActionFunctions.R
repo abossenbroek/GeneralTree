37e72244ccea5bc2995beb0e49bb6f74804011d3
Anton Bossenbroek <anton.bossenbroek@me.com>
2016-08-15 10:14:33 -0400

    Test that delete, deletes all childeren.
    Test whether delete, deletes all childeren.

--- tests/testthat/test_ActionFunctions.R
+++ tests/testthat/test_ActionFunctions.R
@@ -1,4 +1,4 @@
-context('Test action functions')
+context("Test action functions")
 
 test_that("have_siblings is working as expected", {
   tree <- GeneralTree$new(0, "parent1")
@@ -12,15 +12,39 @@ test_that("have_siblings is working as expected", {
   expect_identical(tree$searchNode(2)$have_siblings, TRUE)
 })
 
-test_that('is_last_sibling is working as expected', {
-  tree <- GeneralTree$new(0, 'parent1')
+test_that("is_last_sibling is working as expected", {
+  tree <- GeneralTree$new(0, "parent1")
 
   expect_identical(tree$have_siblings, FALSE)
 
-  tree$addNode(0, 1, 'child.1')
-  tree$addNode(0, 2, 'child.2')
-  tree$addNode(0, 3, 'child.3')
+  tree$addNode(0, 1, "child.1")
+  tree$addNode(0, 2, "child.2")
+  tree$addNode(0, 3, "child.3")
 
   expect_identical(tree$searchNode(2)$is_last_sibling, FALSE)
   expect_identical(tree$searchNode(3)$is_last_sibling, TRUE)
 })
+
+test_that("setData is working as expected", {
+  tree <- GeneralTree$new(0, "parent1")
+
+  tree$addNode(0, 1, "child.1")
+  tree$addNode(0, 2, "child.2")
+  tree$addNode(0, 3, "child.3")
+
+  tree$searchNode(2)$setData("new data for child 2")
+
+  expect_identical(tree$searchNode(2)$data, "new data for child 2")
+})
+
+test_that("search is working as expected", {
+  tree <- GeneralTree$new(0, "parent1")
+
+  tree$addNode(0, 1, "child.1")
+  tree$addNode(0, 2, "child.2")
+  tree$addNode(0, 3, "child.3")
+
+  expect_identical(tree$search(2), "child.2")
+})
+
+
--- tests/testthat/test_delete.R
+++ tests/testthat/test_delete.R
@@ -83,3 +83,21 @@ test_that('delete node that is three levels deep and left child', {
   expect_identical(tree$searchNode(9)$data, 'child.7.9')
   expect_identical(tree$searchNode(10)$data, 'child.7.10')
 })
+
+test_that('delete node that has childeren.', {
+  tree <- GeneralTree$new(0, 'parent1')
+
+  tree$addNode(0, 1, 'child.1')
+  tree$addNode(0, 2, 'child.2')
+  tree$addNode(0, 3, 'child.3')
+  tree$addNode(3, 4, 'child.3.4')
+  tree$addNode(3, 5, 'child.3.5')
+  tree$addNode(3, 6, 'child.3.6')
+  tree$addNode(3, 7, 'child.3.7')
+  tree$addNode(7, 8, 'child.7.8')
+  tree$addNode(7, 9, 'child.7.9')
+  tree$addNode(7, 10, 'child.7.10')
+
+  tree$searchNode(7)$delete()
+  expect_identical(tree$searchNode(8), NULL)
+})
